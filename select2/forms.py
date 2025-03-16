95bec4391cd90faaf61da83f81fa482420fc730a
agneswanderlei <agneswanderlei@gmail.com>
2025-03-15 21:01:31 -0300

    atualizacao da classe pessoascreateview

--- select2/forms.py
+++ select2/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Pessoas
-from dal import autocomplete
 
 # class PessoasForm(forms.ModelForm):
 #     class Meta:
--- select2/views.py
+++ select2/views.py
@@ -2,7 +2,6 @@ from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from .models import Pessoas, Estados
 from .forms import PessoasForm
-from dal import autocomplete
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.db.models import Q
@@ -12,20 +11,15 @@ class PessoasCreateView(CreateView):
     model = Pessoas  # Define o modelo usado pela CreateView
     form_class = PessoasForm  # Usa o formulário personalizado
     template_name = 'pessoas_form.html'  # Nome do template a ser renderizado
-    success_url = reverse_lazy('pessoas_list')  # Redireciona após o sucesso
+    success_url = reverse_lazy('pessoas_create')  # Redireciona após o sucesso
+    def dispatch(self, request, *args, **kwargs):
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # Verifica se é uma requisição AJAX
+            term = request.GET.get('q')
+            estados = Estados.objects.filter(Q(name__icontains=term) | Q(uf__icontains=term))
+            response_content = list(estados.values())
+            return JsonResponse(response_content, safe=False)
+        return super().dispatch(request, *args, **kwargs)
 
-
-
-class EstadosAutocomplete(autocomplete.Select2QuerySetView):
-    def get_queryset(self):
-        # Certifique-se de que o usuário esteja autenticado (se necessário)
-       
-        qs = Estados.objects.all()
-        print(self.term)
-        if self.q:  # `self.q` contém o termo digitado pelo usuário
-            qs = qs.filter(name__icontains=self.q)  # Busca pelo nome do estado
-
-        return qs
 
 def home(request):
     form = PessoasForm()
